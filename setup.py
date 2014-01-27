664fef07fad69863ae961b1fd30ff04b79bc77ae
Simone Federici <sfederici@geniusbytes.com>
2014-01-27 22:25:04 +0100

    import from pycon_site deps dir

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='email_template',
+    version='0.1',
+    description='django email_template extracted from fork of pycon_site',
+    author='dvd',
+    author_email='dvd@develer.com',
+    packages=[
+        'email_template',
+    ],
+    package_data={
+    },
+    install_requires=[
+    ],
+)
